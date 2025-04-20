5909ed379796ab92648813d67e4ec3448350ef46
samaont <samaon1234@gmail.com>
2025-04-20 17:34:37 +0300

    add test

---
+++ Tests/Arrays.spec.js
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+
+describe('Array test suit', ()=>{
+
+    describe('Test the JS works good', ()=>{
+        it('should sort array by name', ()=>{
+            const names =['adiel','tom','john']
+            expect(names.sort()).to.be.eql(['adiel','john','tom']);
+        });
+
+        it('should sort array by name',()=>{
+            const names =['bob','alice','chris']
+            expect(names.sort()).to.be.eql(['alice','bob','chris']);
+        })
+
+  })
+})

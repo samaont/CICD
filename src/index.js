2b84ee4a3b847c7947eeb93ed76ecc30b36eeb6c
samaont <samaon1234@gmail.com>
2025-04-20 15:57:28 +0300

    add change

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+function react(req, res){
+    console.log("React function hasbeen called")
+    res.send('Hello World!');
+
+}
+
+app.get('/', react)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
